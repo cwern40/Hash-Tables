5899f97e1adecda8cb8373e13dbfd0986ad6e56f
cwern40 <chris.wernli@hotmail.com>
2020-04-11 11:58:27 -0600

    I impolemented the djb2 hash function

--- src/hashtable.py
+++ src/hashtable.py
@@ -28,12 +28,10 @@ class HashTable:
 
 
     def _hash_djb2(self, key):
-        '''
-        Hash an arbitrary key using DJB2 hash
-
-        OPTIONAL STRETCH: Research and implement DJB2
-        '''
-        pass
+        djb2_hash = 5381
+        for x in key:
+            djb2_hash = (( djb2_hash << 5) + djb2_hash) + ord(x)
+        return djb2_hash
 
 
     def _hash_mod(self, key):
@@ -41,7 +39,7 @@ class HashTable:
         Take an arbitrary key and return a valid integer index
         within the storage capacity of the hash table.
         '''
-        return self._hash(key) % self.capacity
+        return self._hash_djb2(key) % self.capacity
 
 
     def insert(self, key, value):
